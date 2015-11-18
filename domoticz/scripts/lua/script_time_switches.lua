1f4766d70b99863f197d46972e90065d0817ff5c
erik4281 <erik4281@users.noreply.github.com>
2015-11-18 21:54:15 +0100

    Update script_time_switches.lua

--- domoticz/scripts/lua/script_time_switches.lua
+++ domoticz/scripts/lua/script_time_switches.lua
@@ -3,16 +3,12 @@ commandArray = {}
 time = os.date("*t")
 
 for i, v in pairs(otherdevices) do
-
 	tc = tostring(i)
 	v = i:sub(1,6)
-
 	if (v == 'Switch') then
-
 		c = i:sub(7)
 		t = 'Switch'..c
 		scriptfolder = "/home/pi/domoticz/scripts/bash/"
-
 		if (otherdevices[t] == 'On') then
 			timenumber = tonumber(os.date("%H")..os.date("%M"))
 			time0 = tonumber(uservariables['Timer'..c..'0'])
@@ -21,13 +17,11 @@ for i, v in pairs(otherdevices) do
 			time3 = tonumber(uservariables['Timer'..c..'3'])
 			time4 = tonumber(uservariables['Timer'..c..'4'])
 			execute = 0
-
 			if (time1) then
 				scene = 0
 			else
 				scene = 1
 			end
-
 			if (time0) then
 				if (timenumber == time0) then
 					scene = 0
@@ -58,18 +52,15 @@ for i, v in pairs(otherdevices) do
 					execute = 1
 				end
 			end
-
 			if (execute == 1) then
 				scene = scene..'Slow.sh'
 				print ('Time triggered: '..scriptfolder..c..'/'..scene)
 				os.execute (scriptfolder..c..'/'..scene)
+				os.execute (scriptfolder..c..'/'..scene)
 				execute = 0
 			end
-
 		end
-
 	end
-
 end
 
 return commandArray
