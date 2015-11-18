bb675eecd4014d8a73dd01bc275a96c66ecebdf9
erik4281 <erik4281@users.noreply.github.com>
2015-11-18 19:21:20 +0100

    Create script_device_mode_wake.lua

---
+++ domoticz/scripts/lua/script_device_mode_wake.lua
@@ -0,0 +1,25 @@
+commandArray = {}
+
+t = next(devicechanged)
+s = tostring(t)
+sleepswitch = otherdevices['SleepMode']
+sleepswitchname = 'SleepMode'
+
+if (s:sub(1,6) == 'Motion' and otherdevices['People'] == 'On' and sleepswitch == 'On') then
+
+	timenumber = tonumber(os.date("%H")..os.date("%M"))
+	wakestart = 0600
+	wakestop = 1200
+
+	if (timenumber >= wakestart and timenumber < wakestop) then
+		c = s:sub(7)
+		scriptfolder = "/home/pi/domoticz/scripts/bash/"
+		if (c == 'Living' or c == 'Dining' or c == 'Kitchen') then
+			print ("Waking up")
+			commandArray[sleepswitchname] = 'Off'
+		end
+	end
+
+end
+
+return commandArray
