47d7e89643079e887be2d58aa8d1b91cb5e8a977
erik4281 <erik4281@users.noreply.github.com>
2015-11-18 21:50:18 +0100

    Update script_device_switches.lua

--- domoticz/scripts/lua/script_device_switches.lua
+++ domoticz/scripts/lua/script_device_switches.lua
@@ -5,25 +5,20 @@ t = next(devicechanged)
 s = tostring(t)
 
 if (s:sub(1,6) == 'Switch') then
-
 	c = s:sub(7)
 	t = 'Switch'..c
 	scriptfolder = "/home/pi/domoticz/scripts/bash/"
-
 	if (otherdevices[t] == 'On') then
-
 		timenumber = tonumber(os.date("%H")..os.date("%M"))
 		time1 = tonumber(uservariables['Timer'..c..'1'])
 		time2 = tonumber(uservariables['Timer'..c..'2'])
 		time3 = tonumber(uservariables['Timer'..c..'3'])
 		time4 = tonumber(uservariables['Timer'..c..'4'])
-
 		if (time1) then
 			scene = 0
 		else
 			scene = '1'
 		end
-
 		if (scene == 0 and time1) then
 			if (timenumber >= time1) then
 				scene = 1
@@ -47,16 +42,13 @@ if (s:sub(1,6) == 'Switch') then
 		if (otherdevices['SleepMode'] == 'On') then
 			scene = 9
 		end
-
 	else
 		scene = 'Off'
 	end
-
 	scene = scene..'.sh'
 	print ('Switch triggered: '..scriptfolder..c..'/'..scene)
 	os.execute (scriptfolder..c..'/'..scene)
 	os.execute (scriptfolder..c..'/'..scene)
-
 end
 
 return commandArray
