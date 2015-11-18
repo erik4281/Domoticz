77f8615803d9d294dff7a75b9a33f625a1e13c6f
erik4281 <erik4281@users.noreply.github.com>
2015-11-18 19:47:36 +0100

    Create script_device_motion.lua

---
+++ domoticz/scripts/lua/script_device_motion.lua
@@ -0,0 +1,104 @@
+commandArray = {}
+
+t = next(devicechanged)
+s = tostring(t)
+
+if (s:sub(1,6) == 'Motion' and otherdevices['People'] == 'On') then
+
+	c = s:sub(7)
+	d = nil
+	e = nil
+	f = nil
+	l = nil
+	m = nil
+
+	if (c == 'FrontDoor') then
+		commandArray['Variable:MotionTrigger'] = '0'
+	end
+
+	if (c == 'Living') then
+		c = 'Living'
+		d = 'Dining'
+		l = 'LivingExtra'
+	elseif (c == 'Dining') then
+		c = 'Living'
+		d = 'Dining'
+		l = 'LivingExtra'
+	elseif (c == 'Kitchen') then
+		c = 'Kitchen'
+		d = 'Living'
+		e = 'Dining'
+		l = 'LivingExtra'
+		m = 'KitchenExtra'
+	elseif (c == 'FrontDoor') then
+		c = 'Hallway'
+		d = 'FrontDoor'
+	elseif (c == 'Toilet') then
+		c = 'Toilet'
+		d = 'Hallway'
+	elseif (c == 'Bathroom') then
+		c = 'Bathroom'
+		d = 'Hallway'
+	end
+
+	t = 'Motion'..c
+	cbr = tonumber(otherdevices_svalues['Brightness'..c])
+	cbrt = tonumber(uservariables['BrightnessTrigger'..c])
+	
+	if (d) then
+		u = 'Motion'..d
+	end
+	if (e) then
+		v = 'Motion'..e
+	end
+
+	if (otherdevices[t] == 'On' or otherdevices[u] == 'On' or otherdevices[v] == 'On') then
+		commandArray['Variable:MotionTrigger']= '1'
+		if (cbr < cbrt) then
+			if (otherdevices['Switch'..c] == 'Off') then
+				print (s..' saw motion. Now triggering switch Switch'..c)
+				commandArray['Switch'..c] = 'On'
+			end
+			if (d and otherdevices['Switch'..d] == 'Off') then
+				print (s..' saw motion. Now triggering switch Switch'..d)
+				commandArray['Switch'..d] = 'On'
+			end
+			if (e and otherdevices['Switch'..e] == 'Off') then
+				print (s..' saw motion. Now triggering switch Switch'..e)
+				commandArray['Switch'..e] = 'On'
+			end
+			if (l and otherdevices['Switch'..l] == 'Off') then
+				print (s..' saw motion. Now triggering switch Switch'..l)
+				commandArray['Switch'..l] = 'On'
+			end
+			if (m and otherdevices['Switch'..m] == 'Off') then
+				print (s..' saw motion. Now triggering switch Switch'..m)
+				commandArray['Switch'..m] = 'On'
+			end
+		else
+			if (otherdevices['Switch'..c] == 'On') then
+				print (s..' saw motion. Now triggering switch Switch'..c)
+				commandArray['Switch'..c] = 'Off'
+			end
+			if (d and otherdevices['Switch'..d] == 'On') then
+				print (s..' saw motion. Now triggering switch Switch'..d)
+				commandArray['Switch'..d] = 'Off'
+			end
+			if (e and otherdevices['Switch'..e] == 'On') then
+				print (s..' saw motion. Now triggering switch Switch'..e)
+				commandArray['Switch'..e] = 'Off'
+			end
+			if (l and otherdevices['Switch'..l] == 'Off') then
+				print (s..' saw motion. Now triggering switch Switch'..l)
+				commandArray['Switch'..l] = 'On'
+			end
+			if (m and otherdevices['Switch'..m] == 'Off') then
+				print (s..' saw motion. Now triggering switch Switch'..m)
+				commandArray['Switch'..m] = 'On'
+			end
+		end
+	end
+
+end
+
+return commandArray
