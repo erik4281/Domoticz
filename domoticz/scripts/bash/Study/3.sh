1c24d13d4a4aefcf77a4514488e505f837a0ee2e
erik4281 <erik4281@users.noreply.github.com>
2015-11-20 17:31:25 +0100

    Update 3.sh

--- domoticz/scripts/bash/Study/3.sh
+++ domoticz/scripts/bash/Study/3.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 200,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/20/state
+sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255}' http://10.0.1.102/api/erikvennink/lights/21/state
