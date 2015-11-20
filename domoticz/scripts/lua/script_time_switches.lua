30c91041831ea813b2e54a80c2c4cdd8758b7e2f
erik4281 <erik4281@users.noreply.github.com>
2015-11-20 19:10:21 +0100

    Update script_time_switches.lua

--- domoticz/scripts/lua/script_time_switches.lua
+++ domoticz/scripts/lua/script_time_switches.lua
@@ -1,3 +1,16 @@
+function timedifference(s)
+	year = string.sub(s, 1, 4)
+	month = string.sub(s, 6, 7)
+	day = string.sub(s, 9, 10)
+	hour = string.sub(s, 12, 13)
+	minutes = string.sub(s, 15, 16)
+	seconds = string.sub(s, 18, 19)
+	t1 = os.time()
+	t2 = os.time{year=year, month=month, day=day, hour=hour, min=minutes, sec=seconds}
+	difference = os.difftime (t1, t2)
+	return difference
+end
+ 
 commandArray = {}
 
 time = os.date("*t")
@@ -10,6 +23,7 @@ for i, v in pairs(otherdevices) do
 		t = 'Switch'..c
 		scriptfolder = "/home/pi/domoticz/scripts/bash/"
 		if (otherdevices[t] == 'On') then
+			difference = timedifference(otherdevices_lastupdate[t])
 			timenumber = tonumber(os.date("%H")..os.date("%M"))
 			time0 = tonumber(uservariables['Timer'..c..'0'])
 			time1 = tonumber(uservariables['Timer'..c..'1'])
@@ -17,6 +31,39 @@ for i, v in pairs(otherdevices) do
 			time3 = tonumber(uservariables['Timer'..c..'3'])
 			time4 = tonumber(uservariables['Timer'..c..'4'])
 			execute = 0
+			if (difference >= 1 and difference < 61) then
+				if (time1) then
+					scene = 0
+				else
+					scene = '1'
+				end
+				if (scene == 0 and time1) then
+					if (timenumber >= time1) then
+						scene = 1
+					end
+				end
+				if (scene == 1 and time2) then
+					if (timenumber >= time2) then
+						scene = 2
+					end
+				end
+				if (scene == 2 and time3) then
+					if (timenumber >= time3) then
+						scene = 3
+					end
+				end
+				if (scene == 3 and time4) then
+					if (timenumber >= time4) then
+						scene = 4
+					end
+				end
+				if (otherdevices['SleepMode'] == 'On') then
+					scene = 9
+				end
+			end
+			scene = scene..'.sh'
+			print ('Backup triggered: '..scriptfolder..c..'/'..scene)
+			os.execute (scriptfolder..c..'/'..scene)
 			if (time1) then
 				scene = 0
 			else
@@ -58,6 +105,14 @@ for i, v in pairs(otherdevices) do
 				os.execute (scriptfolder..c..'/'..scene)
 				execute = 0
 			end
+		elseif (otherdevices[t] == 'Off') then
+			difference = timedifference(otherdevices_lastupdate[t])
+			if (difference >= 1 and difference < 61) then
+				scene = 'Off'
+			end
+			scene = scene..'.sh'
+			print ('Backup triggered: '..scriptfolder..c..'/'..scene)
+			os.execute (scriptfolder..c..'/'..scene)
 		end
 	end
 end
