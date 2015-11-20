fd2e3ce6569987c1056c02f5d13d309e245e3b47
erik4281 <erik4281@users.noreply.github.com>
2015-11-20 14:03:04 +0100

    Update script_device_mode_arrive.lua

--- domoticz/scripts/lua/script_device_mode_arrive.lua
+++ domoticz/scripts/lua/script_device_mode_arrive.lua
@@ -11,13 +11,11 @@ if (s:sub(1,6) == 'Motion' and c == 'FrontDoor' and devicechanged[t] == 'On' and
 	commandArray[presencewitchname] = 'On'
 	commandArray['TriggerDoor'] = 'Off'
 	os.execute ('/home/pi/domoticz/scripts/bash/Hallway/1.sh')
-	os.execute ('/home/pi/domoticz/scripts/bash/Hallway/1.sh')
 end
 if (s:sub(1,6) == 'Motion' and c == 'Bedroom' and devicechanged[t] == 'On' and presenceswitch == 'Off') then
 	print ("Movement in Bedroom. Switching to present again!")
 	commandArray[presencewitchname] = 'On'
 	commandArray['TriggerDoor'] = 'Off'
-	os.execute ('/home/pi/domoticz/scripts/bash/Bedroom/0.sh')
 	os.execute ('/home/pi/domoticz/scripts/bash/Bedroom/0.sh')
 end
 
