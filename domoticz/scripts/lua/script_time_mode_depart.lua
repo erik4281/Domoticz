96fd7eab3bda9a20b13dc8dd4467d94f1b81da1e
erik4281 <erik4281@users.noreply.github.com>
2015-11-20 13:48:31 +0100

    Update script_time_mode_depart.lua

--- domoticz/scripts/lua/script_time_mode_depart.lua
+++ domoticz/scripts/lua/script_time_mode_depart.lua
@@ -26,8 +26,7 @@ for i, v in pairs(otherdevices) do
 		timeon = uservariables['DepartTimer']
 		difference = timedifference(otherdevices_lastupdate[tc])
 		timewait = timeon * 60
-		--if (presenceswitch == "On" and door == 'Off' and otherdevices['TriggerDoor'] == 'On' and difference >= timewait and difference < (timewait + 600)) then
-		if (presenceswitch == "On" and door == 'Off' and otherdevices['TriggerDoor'] == 'On' and difference >= timewait) then
+		if (presenceswitch == "On" and door == 'Off' and otherdevices['TriggerDoor'] == 'On' and difference >= timewait and difference < (timewait + 600)) then
 			print ("Departing")
 			commandArray['TriggerDoor']='Off'
 			commandArray[presenceswitchname]='Off'
