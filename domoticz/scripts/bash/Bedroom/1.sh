dbb78bed444c8ae4c7ed1a2188067a67f82f9407
erik4281 <erik4281@users.noreply.github.com>
2015-11-20 17:22:12 +0100

    Update 1.sh

--- domoticz/scripts/bash/Bedroom/1.sh
+++ domoticz/scripts/bash/Bedroom/1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/16/state
+sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":25000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/17/state
+sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":0,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/18/state
+sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255}' http://10.0.1.102/api/erikvennink/lights/19/state
