c49a4a0f870c10616e273c72b031c60f8c4d7547
erik4281 <erik4281@users.noreply.github.com>
2015-11-21 13:13:45 +0100

    Update script_device_switches_mode.lua

--- domoticz/scripts/lua/script_device_switches_mode.lua
+++ domoticz/scripts/lua/script_device_switches_mode.lua
@@ -11,14 +11,20 @@ if (dc == 'SleepMode' or dc == 'People') then
 			c = i:sub(7)
 			t = 'Switch'..c
 			scriptfolder = "/home/pi/domoticz/scripts/bash/"
-			if (dc == 'SleepMode' and otherdevices[dc] == 'On' and otherdevices[t] == 'On') then
-				scene = '9Slow.sh'
-				print ('SleepMode triggered sleep: '..scriptfolder..c..'/'..scene)
-				os.execute (scriptfolder..c..'/'..scene)
-			elseif (dc == 'SleepMode' and otherdevices[t] == 'On') then
-				scene = '1Slow.sh'
-				print ('SleepMode triggered wake: '..scriptfolder..c..'/'..scene)
-				os.execute (scriptfolder..c..'/'..scene)
+			if (dc == 'SleepMode' and otherdevices[dc] == 'On') then
+				commandArray['Bedroom Humidifier'] = 'On'
+				if (otherdevices[t] == 'On') then
+					scene = '9Slow.sh'
+					print ('SleepMode triggered sleep: '..scriptfolder..c..'/'..scene)
+					os.execute (scriptfolder..c..'/'..scene)
+				end
+			elseif (dc == 'SleepMode' and otherdevices[dc] == 'Off') then
+				commandArray['Bedroom Humidifier'] = 'Off'
+				if (otherdevices[t] == 'On') then
+					scene = '1Slow.sh'
+					print ('SleepMode triggered wake: '..scriptfolder..c..'/'..scene)
+					os.execute (scriptfolder..c..'/'..scene)
+				end
 			elseif (dc == 'People' and otherdevices[dc] == 'Off') then
 				scene = 'Off.sh'
 				print ('Presence triggered away: '..scriptfolder..c..'/'..scene)
