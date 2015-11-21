7735cedb550eb50543b657e18a3c78b51dbfa7f8
erik4281 <erik4281@users.noreply.github.com>
2015-11-21 12:30:11 +0100

    Update 9.sh

--- domoticz/scripts/bash/Bedroom/9.sh
+++ domoticz/scripts/bash/Bedroom/9.sh
@@ -3,6 +3,4 @@ curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 100,"sat"
 sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":false,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/17/state
 sleep .05
-curl -s -H "Accept: application/json" -X PUT --data '{"on":false,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/18/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":false}' http://10.0.1.102/api/erikvennink/lights/19/state
