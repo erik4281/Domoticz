200ef2437a613b6054511c13931b1adb639aeafe
erik4281 <erik4281@users.noreply.github.com>
2015-11-21 10:21:12 +0100

    Update script_time_mode_depart.lua

--- domoticz/scripts/lua/script_time_mode_depart.lua
+++ domoticz/scripts/lua/script_time_mode_depart.lua
@@ -14,7 +14,6 @@ end
 commandArray = {}
 
 door = otherdevices['MotionFrontDoor']
-doortime = otherdevices_lastupdate['MotionFrontDoor']
 presenceswitch = otherdevices['People']
 presenceswitchname = 'People'
 erik = otherdevices['iPhoneErik']
@@ -24,13 +23,54 @@ for i, v in pairs(otherdevices) do
 	tc = tostring(i)
 	v = i:sub(1,6)
 	c = i:sub(7)
-	if (v == 'Motion' and c == 'FrontDoor') then
+	if (tc == 'MotionFrontDoor') then
 		timeon = uservariables['DepartTimer']
 		difference = timedifference(otherdevices_lastupdate[tc])
-		timewait = (timeon * 60) + 60
-		if (presenceswitch == "On" and erik == 'Off' and jinhee = 'Off' and door == 'Off' and difference >= timewait and difference < (timewait + 60)) then
-			print ("Departing")
-			commandArray[presenceswitchname]='Off'
+		timewait = (timeon * 60) + 120
+		if (presenceswitch == "On" and door == 'Off' and difference < (timewait + 60)) then
+			prefix="(PING) "
+			local ping={}
+			local ping_success
+			local bt_success
+			ping[1]={'10.0.1.121', 'iPhoneErik', 'iPhoneErik', 'DC:9B:9C:BE:D5:08', timeon * 60}
+			ping[2]={'10.0.1.122', 'iPhoneJinHee', 'iPhoneJinHee', 'F0:24:75:D0:AF:C2', timeon * 60}
+			for ip = 1, #ping do
+				if ping[ip][4] == 'nil' then
+					bt_success=false
+					ping_success=os.execute('ping -c 1 -w 1 '..ping[ip][1])
+				else
+					f = assert (io.popen ("hcitool names "..ping[ip][4]))
+					bt = f:read()
+					if bt==nil then
+						bt_success=false
+					else
+						bt_success=true
+					end
+					f:close()
+				end
+				if ping_success or bt_success then
+					print(prefix.."ping success "..ping[ip][2])
+					if (otherdevices[ping[ip][2]]=='Off') then
+						commandArray[ping[ip][2]]='On'
+					end
+					if (uservariables[ping[ip][3]]) ~= 1 then
+						commandArray['Variable:'..ping[ip][3]]= tostring(1)
+					end
+				else
+					print(prefix.."ping fail "..ping[ip][2])
+					if (otherdevices[ping[ip][2]]=='On') then
+						if (uservariables[ping[ip][3]])==ping[ip][5] then
+							commandArray[ping[ip][2]]='Off'
+						else
+							commandArray['Variable:'..ping[ip][3]]= tostring((uservariables[ping[ip][3]]) + 1)
+						end
+					end
+				end
+			end
+			if (erik == 'Off' and jinhee == 'Off') then
+				print ("Departing")
+				commandArray[presenceswitchname]='Off'
+			end
 		end
 	end
 end
