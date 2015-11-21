e2e10b4898efc4939cf07a9ef35bcfd971860080
erik4281 <erik4281@users.noreply.github.com>
2015-11-21 09:28:40 +0100

    Update script_device_switches_mode.lua

--- domoticz/scripts/lua/script_device_switches_mode.lua
+++ domoticz/scripts/lua/script_device_switches_mode.lua
@@ -3,7 +3,7 @@ commandArray = {}
 dc = next(devicechanged)
 s = tostring(t)
 
-if (dc == 'SleepMode') then
+if (dc == 'SleepMode' or dc == 'People') then
 	for i, v in pairs(otherdevices) do
 		tc = tostring(i)
 		v = i:sub(1,6)
@@ -11,28 +11,15 @@ if (dc == 'SleepMode') then
 			c = i:sub(7)
 			t = 'Switch'..c
 			scriptfolder = "/home/pi/domoticz/scripts/bash/"
-			if (otherdevices[dc] == 'On' and otherdevices[t] == 'On') then
+			if (dc == 'SleepMode' and otherdevices[dc] == 'On' and otherdevices[t] == 'On') then
 				scene = '9Slow.sh'
 				print ('SleepMode triggered sleep: '..scriptfolder..c..'/'..scene)
 				os.execute (scriptfolder..c..'/'..scene)
-			elseif (otherdevices[t] == 'On') then
+			elseif (dc == 'SleepMode' and otherdevices[t] == 'On') then
 				scene = '1Slow.sh'
 				print ('SleepMode triggered wake: '..scriptfolder..c..'/'..scene)
 				os.execute (scriptfolder..c..'/'..scene)
-			end
-		end
-	end
-end
-
-if (dc == 'People') then
-	for i, v in pairs(otherdevices) do
-		tc = tostring(i)
-		v = i:sub(1,6)
-		if (v == 'Switch') then
-			c = i:sub(7)
-			t = 'Switch'..c
-			scriptfolder = "/home/pi/domoticz/scripts/bash/"
-			if (otherdevices[dc] == 'Off') then
+			elseif (dc == 'People' and otherdevices[dc] == 'Off')
 				scene = 'Off.sh'
 				print ('Presence triggered away: '..scriptfolder..c..'/'..scene)
 				os.execute (scriptfolder..c..'/'..scene)
