20d38ca28c496e242b1a4fdabe4c85896e86e07e
erik4281 <erik4281@users.noreply.github.com>
2015-11-21 18:24:10 +0100

    Update script_device_mode_arrive.lua

--- domoticz/scripts/lua/script_device_mode_arrive.lua
+++ domoticz/scripts/lua/script_device_mode_arrive.lua
@@ -7,14 +7,15 @@ presenceswitch = otherdevices['People']
 presenceswitchname = 'People'
 
 if (s:sub(1,6) == 'Motion' and c == 'FrontDoor' and devicechanged[t] == 'On') then
-	print ("Arriving")
+	print ("Door opening, checking presence of phones")
 	os.execute ('/home/pi/domoticz/scripts/bash/Hallway/1.sh')
 	prefix="(PING) "
 	local ping={}
 	local ping_success
 	local bt_success
-	ping[1]={'10.0.1.121', 'iPhoneErik', 'iPhoneErik', 'DC:9B:9C:BE:D5:08'}
-	ping[2]={'10.0.1.122', 'iPhoneJinHee', 'iPhoneJinHee', 'F0:24:75:D0:AF:C2'}
+	timeon = uservariables['DepartTimer']
+	ping[1]={'10.0.1.121', 'iPhoneErik', 'iPhoneErik', 'DC:9B:9C:BE:D5:08', timeon}
+	ping[2]={'10.0.1.122', 'iPhoneJinHee', 'iPhoneJinHee', 'F0:24:75:D0:AF:C2', timeon}
 	for ip = 1, #ping do
 		if ping[ip][4] == 'nil' then
 			bt_success=false
@@ -39,6 +40,9 @@ if (s:sub(1,6) == 'Motion' and c == 'FrontDoor' and devicechanged[t] == 'On') th
 			end
 			if (otherdevices[ping[ip][2]]=='Off') then
 				commandArray[ping[ip][2]]='On'
+			end
+			if (uservariables[ping[ip][3]]) ~= 1 then
+				commandArray['Variable:'..ping[ip][3]]= tostring(1)
 			end
 		else
 			print(prefix.."ping fail "..ping[ip][2])
