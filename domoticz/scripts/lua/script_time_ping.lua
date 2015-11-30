87d6b2c13cc0645da7d08dd35e98eb2243f0bd4f
erik4281 <erik4281@users.noreply.github.com>
2015-11-30 18:58:39 +0100

    Update script_time_ping.lua

--- domoticz/scripts/lua/script_time_ping.lua
+++ domoticz/scripts/lua/script_time_ping.lua
@@ -1,16 +1,24 @@
 commandArray={}
 
-debug=false
+debug=true
 
 prefix="(PING) "
+--timeon = uservariables['DepartTimer']
+timeon = 2
 
 local ping={}
 local ping_success
 local bt_success
 
-ping[1]={'192.168.1.1', 'Device 1', 'Teller 1', 'nil', 5}                 -- android
-ping[2]={'192.168.1.2', 'Device 2', 'Teller 2', 'aa:bb:cc:dd:ee:ff', 5}   -- iphone
-ping[3]={'192.168.1.3', 'Device 3', 'Teller 3', 'nil', 5}                 -- android
+ping[1]={'10.0.1.123', 'iPadErik', 'iPadErik', timeon}
+ping[2]={'10.0.1.124', 'iPadJinHee', 'iPadJinHee', timeon}
+ping[3]={'10.0.1.125', 'MacBookAirErik', 'MacBookAirErik', timeon}
+ping[4]={'10.0.1.126', 'MacBookAirJinHee', 'MacBookAirJinHee', timeon}
+ping[5]={'10.0.1.114', 'AppleTvLiving', 'AppleTvLiving', timeon}
+ping[6]={'10.0.1.115', 'AppleTvBedroom', 'AppleTvBedroom', timeon}
+ping[7]={'10.0.1.109', 'TvLiving', 'TvLiving', timeon}
+ping[8]={'10.0.1.101', 'PS4', 'PS4', timeon}
+ping[9]={'10.0.1.110', 'Printer', 'Printer', timeon}
 
 for ip = 1, #ping do
 	ping_success=os.execute('ping -c 1 -w 1 '..ping[ip][1])
@@ -29,7 +37,7 @@ for ip = 1, #ping do
 			print(prefix.."ping fail "..ping[ip][2])
 		end
 		if (otherdevices[ping[ip][2]]=='On') then
-			if (uservariables[ping[ip][3]])==ping[ip][5] then
+			if (uservariables[ping[ip][3]])==ping[ip][4] then
 				commandArray[ping[ip][2]]='Off'
 			else
 				commandArray['Variable:'..ping[ip][3]]= tostring((uservariables[ping[ip][3]]) + 1)
