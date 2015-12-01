d384d1ba77980759de863f728cda8d4b228bf795
erik4281 <erik4281@users.noreply.github.com>
2015-12-01 19:08:39 +0100

    BIG Update

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -1,20 +1,21 @@
 commandArray = {}
 
 dc = next(devicechanged)
+ts = tostring(dc)
 
-if (dc == 'SleepMode') then
+if (ts == 'SleepMode') then
 	for i, v in pairs(otherdevices) do
 		v = i:sub(1,6)
 		if (v == 'Switch') then
 			c = i:sub(7)
 			scriptfolder = "/home/pi/domoticz/scripts/bash/"
-			if (otherdevices[dc] == 'On') then
+			if (otherdevices[ts] == 'On') then
 				commandArray['Bedroom Humidifier'] = 'On'
 				if (otherdevices[i] == 'On') then
 					scene = '9Slow.sh'
 					os.execute (scriptfolder..c..'/'..scene)
 				end
-			elseif (otherdevices[dc] == 'Off') then
+			elseif (otherdevices[ts] == 'Off') then
 				commandArray['Bedroom Humidifier'] = 'Off'
 				if (otherdevices[i] == 'On') then
 					scene = '1Slow.sh'
@@ -25,13 +26,13 @@ if (dc == 'SleepMode') then
 	end
 end
 
-if (dc == 'People') then
-	if (otherdevices[dc] == 'On') then
+if (ts == 'People') then
+	if (otherdevices[ts] == 'On') then
 		if (otherdevices['NestAway'] == 'On') then
 			commandArray['NestAway'] = 'Off'
 		end
 		commandArray['SendNotification']='Presence#Activated HOME mode#0#default'
-	elseif (otherdevices[dc] == 'Off') then
+	elseif (otherdevices[ts] == 'Off') then
 		if (otherdevices['NestAway'] == 'Off') then
 			commandArray['NestAway'] = 'On'
 		end
@@ -47,19 +48,17 @@ if (dc == 'People') then
 	end
 end
 
-if (dc == 'ALARM' and otherdevices[dc] == 'On' and otherdevices['People'] == 'Off') then
+if (ts == 'ALARM' and otherdevices[ts] == 'On' and otherdevices['People'] == 'Off') then
 	for i, v in pairs(otherdevices) do
 		v = i:sub(1,6)
-		if (v == 'Switch') then
-			if (v == 'Motion' and otherdevices[i] == 'On') then
-				commandArray['SendNotification']='ALARM#ALARM: '..i..' is ON, but nobody is home!#2#default'
-			end
-			if (v == 'Motion' and otherdevices[i] == 'Open') then
-				commandArray['SendNotification']='ALARM#ALARM: '..i..' is OPEN, but nobody is home!#2#default'
-			end
-			if (v == 'Tamper' and otherdevices[i] == 'On') then
-				commandArray['SendNotification']='ALARM#ALARM: '..i..' is ON, but nobody is home!#2#default'
-			end
+		if (v == 'Motion' and otherdevices[i] == 'On') then
+			commandArray['SendNotification']='ALARM#ALARM: '..i..' is ON, but nobody is home!#2#default'
+		end
+		if (v == 'Motion' and otherdevices[i] == 'Open') then
+			commandArray['SendNotification']='ALARM#ALARM: '..i..' is OPEN, but nobody is home!#2#default'
+		end
+		if (v == 'Tamper' and otherdevices[i] == 'On') then
+			commandArray['SendNotification']='ALARM#ALARM: '..i..' is ON, but nobody is home!#2#default'
 		end
 	end
 end
