eef71e8ad2527534cea83fc0a81925ac5b25074e
erik4281 <erik4281@users.noreply.github.com>
2015-12-01 20:15:13 +0100

    BIG Update

---
+++ domoticz/scripts/lua/script_time_ping.lua
@@ -0,0 +1,74 @@
+commandArray={}
+
+presence = otherdevices['People']
+
+if (presence == 'On') then
+	
+	debug=false
+	
+	prefix="(TIMED PING) "
+	--timeon = uservariables['DepartTimer']
+	timeon = 2
+	
+	local ping={}
+	local ping_success
+	local bt_success
+	
+	ping[1]={'10.0.1.125', 'MacBookAirErik', 'MacBookAirErik', timeon}
+	ping[2]={'10.0.1.126', 'MacBookAirJinHee', 'MacBookAirJinHee', timeon}
+	ping[3]={'10.0.1.109', 'TvLiving', 'TvLiving', timeon}
+	--ping[4]={'10.0.1.123', 'iPadErik', 'iPadErik', timeon}
+	--ping[5]={'10.0.1.124', 'iPadJinHee', 'iPadJinHee', timeon}
+	--ping[6]={'10.0.1.114', 'AppleTvLiving', 'AppleTvLiving', timeon}
+	--ping[7]={'10.0.1.115', 'AppleTvBedroom', 'AppleTvBedroom', timeon}
+	--ping[8]={'10.0.1.101', 'PS4', 'PS4', timeon}
+	--ping[9]={'10.0.1.110', 'Printer', 'Printer', timeon}
+	
+	for ip = 1, #ping do
+		ping_success=os.execute('ping -c 1 -w 1 '..ping[ip][1])
+		if ping_success then
+			if (debug==true) then
+				print(prefix.."ping success "..ping[ip][2])
+			end
+			if (otherdevices[ping[ip][2]]=='Off') then
+				commandArray[ping[ip][2]]='On'
+			end
+			if (uservariables[ping[ip][3]]) ~= 1 then
+				commandArray['Variable:'..ping[ip][3]]= tostring(1)
+			end
+		else
+			if (debug==true) then
+				print(prefix.."ping fail "..ping[ip][2])
+			end
+			if (otherdevices[ping[ip][2]]=='On') then
+				if (uservariables[ping[ip][3]])==ping[ip][4] then
+					commandArray[ping[ip][2]]='Off'
+				else
+					commandArray['Variable:'..ping[ip][3]]= tostring((uservariables[ping[ip][3]]) + 1)
+				end
+			end
+		end
+	end
+else
+
+	ping[1]={'10.0.1.125', 'MacBookAirErik', 'MacBookAirErik', timeon}
+	ping[2]={'10.0.1.126', 'MacBookAirJinHee', 'MacBookAirJinHee', timeon}
+	ping[3]={'10.0.1.109', 'TvLiving', 'TvLiving', timeon}
+	--ping[4]={'10.0.1.123', 'iPadErik', 'iPadErik', timeon}
+	--ping[5]={'10.0.1.124', 'iPadJinHee', 'iPadJinHee', timeon}
+	--ping[6]={'10.0.1.114', 'AppleTvLiving', 'AppleTvLiving', timeon}
+	--ping[7]={'10.0.1.115', 'AppleTvBedroom', 'AppleTvBedroom', timeon}
+	--ping[8]={'10.0.1.101', 'PS4', 'PS4', timeon}
+	--ping[9]={'10.0.1.110', 'Printer', 'Printer', timeon}
+	
+	for ip = 1, #ping do
+		if (debug==true) then
+			print(prefix.."ping skipped "..ping[ip][2])
+		end
+		if (otherdevices[ping[ip][2]]=='On') then
+			commandArray[ping[ip][2]]='Off'
+		end
+	end
+end
+
+return commandArray
