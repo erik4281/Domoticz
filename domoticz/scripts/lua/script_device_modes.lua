cae00db8580f2d18c1d2479df3cdf2d350d6eb7b
erik4281 <erik4281@users.noreply.github.com>
2015-12-01 19:00:20 +0100

    Big UPDATE

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -1,57 +1,64 @@
 commandArray = {}
 
 dc = next(devicechanged)
-s = tostring(t)
 
-if (dc == 'SleepMode' or dc == 'People' or dc == 'ALARM') then
+if (dc == 'SleepMode') then
 	for i, v in pairs(otherdevices) do
-		tc = tostring(i)
 		v = i:sub(1,6)
 		if (v == 'Switch') then
 			c = i:sub(7)
-			t = 'Switch'..c
 			scriptfolder = "/home/pi/domoticz/scripts/bash/"
-			if (dc == 'SleepMode' and otherdevices[dc] == 'On') then
+			if (otherdevices[dc] == 'On') then
 				commandArray['Bedroom Humidifier'] = 'On'
-				if (otherdevices[t] == 'On') then
+				if (otherdevices[i] == 'On') then
 					scene = '9Slow.sh'
-					print ('SleepMode triggered sleep: '..scriptfolder..c..'/'..scene)
 					os.execute (scriptfolder..c..'/'..scene)
 				end
-			elseif (dc == 'SleepMode' and otherdevices[dc] == 'Off') then
+			elseif (otherdevices[dc] == 'Off') then
 				commandArray['Bedroom Humidifier'] = 'Off'
-				if (otherdevices[t] == 'On') then
+				if (otherdevices[i] == 'On') then
 					scene = '1Slow.sh'
-					print ('SleepMode triggered wake: '..scriptfolder..c..'/'..scene)
 					os.execute (scriptfolder..c..'/'..scene)
-				end
-			end
-			if (dc == 'People' and otherdevices[dc] == 'On') then
-				if (otherdevices['NestAway'] == 'On') then
-					print ('Presence triggered home: Nest switched to home')
-					commandArray['NestAway'] = 'Off'
-				end
-				commandArray['SendNotification']='Presence#Activated HOME mode#0#default'
-			elseif (dc == 'People' and otherdevices[dc] == 'Off') then
-				if (otherdevices['NestAway'] == 'Off') then
-					print ('Presence triggered away: Nest switched to away')
-					commandArray['NestAway'] = 'On'
-				end
-				commandArray['SendNotification']='Presence#Activated AWAY mode#0#default'
-				if (otherdevices[t] == 'On') then
-					commandArray[t] = 'Off'
 				end
 			end
 		end
-		if (dc == 'ALARM' and otherdevices[dc] == 'On' and otherdevices['People'] == 'Off') then
-			if (v == 'Motion' and otherdevices[tc] == 'On') then
-				commandArray['SendNotification']='ALARM#ALARM: '..tc..' is ON, but nobody is home!#2#default'
+	end
+end
+
+if (dc == 'People') then
+	if (otherdevices[dc] == 'On') then
+		if (otherdevices['NestAway'] == 'On') then
+			commandArray['NestAway'] = 'Off'
+		end
+		commandArray['SendNotification']='Presence#Activated HOME mode#0#default'
+	elseif (otherdevices[dc] == 'Off') then
+		if (otherdevices['NestAway'] == 'Off') then
+			commandArray['NestAway'] = 'On'
+		end
+		commandArray['SendNotification']='Presence#Activated AWAY mode#0#default'
+		for i, v in pairs(otherdevices) do
+			v = i:sub(1,6)
+			if (v == 'Switch') then
+				if (otherdevices[i] == 'On') then
+					commandArray[i] = 'Off'
+				end
 			end
-			if (v == 'Motion' and otherdevices[tc] == 'Open') then
-				commandArray['SendNotification']='ALARM#ALARM: '..tc..' is OPEN, but nobody is home!#2#default'
+		end
+	end
+end
+
+if (dc == 'ALARM' and otherdevices[dc] == 'On' and otherdevices['People'] == 'Off') then
+	for i, v in pairs(otherdevices) do
+		v = i:sub(1,6)
+		if (v == 'Switch') then
+			if (v == 'Motion' and otherdevices[i] == 'On') then
+				commandArray['SendNotification']='ALARM#ALARM: '..i..' is ON, but nobody is home!#2#default'
 			end
-			if (v == 'Tamper' and otherdevices[tc] == 'On') then
-				commandArray['SendNotification']='ALARM#ALARM: '..tc..' is ON, but nobody is home!#2#default'
+			if (v == 'Motion' and otherdevices[i] == 'Open') then
+				commandArray['SendNotification']='ALARM#ALARM: '..i..' is OPEN, but nobody is home!#2#default'
+			end
+			if (v == 'Tamper' and otherdevices[i] == 'On') then
+				commandArray['SendNotification']='ALARM#ALARM: '..i..' is ON, but nobody is home!#2#default'
 			end
 		end
 	end
