7c1ce34e327da5496c02f05042cba9560cfc5339
erik4281 <erik4281@users.noreply.github.com>
2015-12-02 21:18:40 +0100

    Update script_time_presence_check.lua

--- domoticz/scripts/lua/script_time_presence_check.lua
+++ domoticz/scripts/lua/script_time_presence_check.lua
@@ -15,63 +15,56 @@ commandArray = {}
 
 door = otherdevices['MotionFrontDoor']
 presence = otherdevices['People']
+timeon = uservariables['DepartTimer']
+difference = timedifference(otherdevices_lastupdate['MotionFrontDoor'])
+timewait = (timeon * 60) + 120
 
---for i, v in pairs(otherdevices) do
---	ts = tostring(i)
---	v = i:sub(1,6)
---	if (ts == 'MotionFrontDoor') then
-		timeon = uservariables['DepartTimer']
-		difference = timedifference(otherdevices_lastupdate['MotionFrontDoor'])
-		timewait = (timeon * 60) + 120
---		if (presence == "On" and door == 'Closed' and difference < (timewait + 60)) then
-		if (presence == "On" and door == 'Closed') then
-			prefix="(PING) "
-			local ping={}
-			local ping_success
-			local bt_success
-			ping[1]={'10.0.1.121', 'iPhoneErik', 'iPhoneErik', 'DC:9B:9C:BE:D5:08', timeon}
-			ping[2]={'10.0.1.122', 'iPhoneJinHee', 'iPhoneJinHee', 'F0:24:75:D0:AF:C2', timeon}
-			for ip = 1, #ping do
-				if ping[ip][4] == 'nil' then
-					bt_success=false
-					ping_success=os.execute('ping -c 1 -w 1 '..ping[ip][1])
-				else
-					f = assert (io.popen ("hcitool names "..ping[ip][4]))
-					bt = f:read()
-					if bt==nil then
-						bt_success=false
-					else
-						bt_success=true
-					end
-					f:close()
-				end
-				if ping_success or bt_success then
-					print(prefix.."ping success "..ping[ip][2])
-					if (otherdevices[ping[ip][2]]=='Off') then
-						commandArray[ping[ip][2]]='On'
-					end
-					if (uservariables[ping[ip][3]]) ~= 1 then
-						commandArray['Variable:'..ping[ip][3]]= tostring(1)
-					end
-				else
-					print(prefix.."ping fail "..ping[ip][2])
-					if (otherdevices[ping[ip][2]]=='On') then
-						if (uservariables[ping[ip][3]])==ping[ip][5] then
-							commandArray[ping[ip][2]]='Off'
-						else
-							commandArray['Variable:'..ping[ip][3]]= tostring((uservariables[ping[ip][3]]) + 1)
-						end
-					end
-				end
+if (presence == "On" and door == 'Closed' and difference < (timewait + 60)) then
+	prefix="(PING) "
+	local ping={}
+	local ping_success
+	local bt_success
+	ping[1]={'10.0.1.121', 'iPhoneErik', 'iPhoneErik', 'DC:9B:9C:BE:D5:08', timeon}
+	ping[2]={'10.0.1.122', 'iPhoneJinHee', 'iPhoneJinHee', 'F0:24:75:D0:AF:C2', timeon}
+	for ip = 1, #ping do
+		if ping[ip][4] == 'nil' then
+			bt_success=false
+			ping_success=os.execute('ping -c 1 -w 1 '..ping[ip][1])
+		else
+			f = assert (io.popen ("hcitool names "..ping[ip][4]))
+			bt = f:read()
+			if bt==nil then
+				bt_success=false
+			else
+				bt_success=true
 			end
-			if ((otherdevices['iPhoneErik'] == 'On' or otherdevices['iPhoneJinHee'] == 'On') and otherdevices['People'] == 'Off') then
-				commandArray['People'] = 'On'
+			f:close()
+		end
+		if ping_success or bt_success then
+			print(prefix.."ping success "..ping[ip][2])
+			if (otherdevices[ping[ip][2]]=='Off') then
+				commandArray[ping[ip][2]]='On'
 			end
-			if ((otherdevices['iPhoneErik'] == 'Off' and otherdevices['iPhoneJinHee'] == 'Off') and otherdevices['People'] == 'On') then
-				commandArray['People'] = 'Off'
+			if (uservariables[ping[ip][3]]) ~= 1 then
+				commandArray['Variable:'..ping[ip][3]]= tostring(1)
+			end
+		else
+			print(prefix.."ping fail "..ping[ip][2])
+			if (otherdevices[ping[ip][2]]=='On') then
+				if (uservariables[ping[ip][3]])==ping[ip][5] then
+					commandArray[ping[ip][2]]='Off'
+				else
+					commandArray['Variable:'..ping[ip][3]]= tostring((uservariables[ping[ip][3]]) + 1)
+				end
 			end
 		end
---	end
---end
+	end
+	if ((otherdevices['iPhoneErik'] == 'On' or otherdevices['iPhoneJinHee'] == 'On') and presence == 'Off') then
+		commandArray['People'] = 'On'
+	end
+	if ((otherdevices['iPhoneErik'] == 'Off' and otherdevices['iPhoneJinHee'] == 'Off') and presence == 'On') then
+		commandArray['People'] = 'Off'
+	end
+end
 
 return commandArray
