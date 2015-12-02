e2ec35d82ff85727de7da9ee505f8bb856cbdac8
erik4281 <erik4281@users.noreply.github.com>
2015-12-02 19:33:56 +0100

    Update script_device_motion_away.lua

--- domoticz/scripts/lua/script_device_motion_away.lua
+++ domoticz/scripts/lua/script_device_motion_away.lua
@@ -17,10 +17,14 @@ if ((ts:sub(1,6) == 'Motion' or ts:sub(1,6) == 'Tamper') and presence == 'Off' a
 	timeon = uservariables['DepartTimer']
 	ping[1]={'10.0.1.121', 'iPhoneErik', 'iPhoneErik', 'DC:9B:9C:BE:D5:08', timeon}
 	ping[2]={'10.0.1.122', 'iPhoneJinHee', 'iPhoneJinHee', 'F0:24:75:D0:AF:C2', timeon}
+	check = 0
 	for ip = 1, #ping do
 		if ping[ip][4] == 'nil' then
 			bt_success=false
 			ping_success=os.execute('ping -c 1 -w 1 '..ping[ip][1])
+			if ping_success then
+				check = 1
+			end
 		else
 			f = assert (io.popen ("hcitool names "..ping[ip][4]))
 			bt = f:read()
@@ -28,6 +32,7 @@ if ((ts:sub(1,6) == 'Motion' or ts:sub(1,6) == 'Tamper') and presence == 'Off' a
 				bt_success=false
 			else
 				bt_success=true
+				check = 1
 			end
 			f:close()
 		end
@@ -39,25 +44,25 @@ if ((ts:sub(1,6) == 'Motion' or ts:sub(1,6) == 'Tamper') and presence == 'Off' a
 			if (uservariables[ping[ip][3]]) ~= 1 then
 				commandArray['Variable:'..ping[ip][3]]= tostring(1)
 			end
+			if (check = 1) then
+				if (otherdevices['People'] == 'Off') then
+					commandArray['People'] = 'On'
+				end
+				if (otherdevices['ALARM'] == 'On') then
+					commandArray['ALARM'] = 'Off'
+				end
+			end
 		else
 			print(prefix.."ping fail "..ping[ip][2])
+			if (check = 0) then
+				if (otherdevices['People'] == 'On') then
+					commandArray['People'] = 'Off'
+				end
+				if (otherdevices['ALARM'] == 'Off') then
+					commandArray['ALARM'] = 'On'
+				end
+			end
 		end
-	end
-	if (otherdevices['iPhoneErik'] == 'On' or otherdevices['iPhoneJinHee'] == 'On') then
-		--if (otherdevices['People'] == 'Off') then
-			commandArray['People'] = 'On'
-		--end
-		--if (otherdevices['ALARM'] == 'On') then
-			commandArray['ALARM'] = 'Off'
-		--end
-	end
-	if (otherdevices['iPhoneErik'] == 'Off' and otherdevices['iPhoneJinHee'] == 'Off') then
-		--if (otherdevices['People'] == 'On') then
-			commandArray['People'] = 'Off'
-		--end
-		--if (otherdevices['ALARM'] == 'Off') then
-			commandArray['ALARM'] = 'On'
-		--end
 	end
 elseif ((ts:sub(1,6) == 'Motion' or ts:sub(1,6) == 'Tamper') and presence == 'Off' and (devicechanged[dc] == 'Off' or devicechanged[dc] == 'Closed')) then
 	if (otherdevices['ALARM'] == 'On') then
