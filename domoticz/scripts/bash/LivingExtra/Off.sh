7b174c249c1ca637bb57cb4ef2d34e5a602064df
erik4281 <erik4281@users.noreply.github.com>
2015-12-05 17:16:30 +0100

    Update Off.sh

--- domoticz/scripts/bash/LivingExtra/Off.sh
+++ domoticz/scripts/bash/LivingExtra/Off.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":false,"transitiontime":50}' http://10.0.1.102/api/erikvennink/lights/1/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":false,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/12/state
