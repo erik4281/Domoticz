2776cd16d7e29b3bc0ab54046191a9a50dacff86
erik4281 <erik4281@users.noreply.github.com>
2015-12-05 07:54:27 +0100

    Update script_device_modes.lua

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -7,7 +7,7 @@ if (ts == 'SleepMode') then
 	for i, v in pairs(otherdevices) do
 		v = i:sub(1,6)
 		if (v == 'Switch') then
-			c = i:sub(7)
+			--c = i:sub(7)
 			--scriptfolder = "/home/pi/domoticz/scripts/bash/"
 			if (devicechanged[dc] == 'On') then
 				commandArray['Bedroom Humidifier'] = 'On'
@@ -26,13 +26,53 @@ if (ts == 'SleepMode') then
 					--os.execute (scriptfolder..c..'/'..scene)
 				--end
 			end
-			if (otherdevices[i] == 'On') then
-				print('Switching lights '..i..' to comply to new mode')
-				commandArray[otherdevices[i]] = 'On'
-				commandArray[otherdevices['SwitchLiving']] = 'On'
+			--if (otherdevices[i] == 'On') then
+				--print('Switching lights '..i..' to comply to new mode')
+				--commandArray[otherdevices[i]] = 'On'
+				--commandArray[otherdevices['SwitchLiving']] = 'On'
 				--scene = '9Slow.sh'
 				--os.execute (scriptfolder..c..'/'..scene)
+			--end
+			sc = i:sub(7)
+			scriptfolder = "/home/pi/domoticz/scripts/bash/"
+			if (otherdevices[i] == 'On') then
+				timenumber = tonumber(os.date("%H")..os.date("%M"))
+				time1 = tonumber(uservariables['Timer'..sc..'1'])
+				time2 = tonumber(uservariables['Timer'..sc..'2'])
+				time3 = tonumber(uservariables['Timer'..sc..'3'])
+				time4 = tonumber(uservariables['Timer'..sc..'4'])
+				if (time1) then
+					scene = 0
+				else
+					scene = 1
+				end
+				if (scene == 0 and time1) then
+					if (timenumber >= time1) then
+						scene = 1
+					end
+				end
+				if (scene == 1 and time2) then
+					if (timenumber >= time2) then
+						scene = 2
+					end
+				end
+				if (scene == 2 and time3) then
+					if (timenumber >= time3) then
+						scene = 3
+					end
+				end
+				if (scene == 3 and time4) then
+					if (timenumber >= time4) then
+						scene = 4
+					end
+				end
+				if (devicechanged[dc] == 'On') then
+					scene = 9
+				end
 			end
+			scene = scene..'.sh'
+			print ('Switch triggered: '..scriptfolder..sc..'/'..scene)
+			os.execute (scriptfolder..sc..'/'..scene)
 		end
 	end
 end
