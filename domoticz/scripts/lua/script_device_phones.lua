593dffe22c8d8ab7aec933d1366fc7f47ff5792c
erik4281 <erik4281@users.noreply.github.com>
2015-12-07 21:43:02 +0100

    Update script_device_phones.lua

--- domoticz/scripts/lua/script_device_phones.lua
+++ domoticz/scripts/lua/script_device_phones.lua
@@ -1,10 +1,20 @@
 commandArray = {}
 
 if (otherdevices['iPhoneErik'] == 'On' or otherdevices['iPhoneJinHee'] == 'On') then
-	commandArray['Phones'] = 'On'
+	if (otherdevices['Phones' == 'Off') then
+		commandArray['Phones'] = 'On'
+	end
+	if (otherdevices['People' == 'Off') then
+		commandArray['People'] = 'On'
+	end
 end
 if (otherdevices['iPhoneErik'] == 'Off' and otherdevices['iPhoneJinHee'] == 'Off') then
-	commandArray['Phones'] = 'Off'
+	if (otherdevices['Phones' == 'On') then
+		commandArray['Phones'] = 'Off'
+	end
+	if (otherdevices['People' == 'On') then
+		commandArray['People'] = 'Off'
+	end
 end
 
 return commandArray
