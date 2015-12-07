a65cf7ddea0ba2cb5824d50d1a25ba7f1f60a7fb
erik4281 <erik4281@users.noreply.github.com>
2015-12-07 22:47:14 +0100

    Update script_device_modes.lua

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -85,12 +85,12 @@ if (ts == 'ALARM' and devicechanged[dc] == 'On' and otherdevices['People'] == 'O
 		v = i:sub(1,6)
 		if (v == 'Motion' and otherdevices[i] == 'On') then
 			commandArray['SendNotification']='ALARM#ALARM: '..i..' is ON, but nobody is home!#2#default'
-		end
-		if (v == 'Motion' and otherdevices[i] == 'Open') then
+		elseif (v == 'Motion' and otherdevices[i] == 'Open') then
 			commandArray['SendNotification']='ALARM#ALARM: '..i..' is OPEN, but nobody is home!#2#default'
-		end
-		if (v == 'Tamper' and otherdevices[i] == 'On') then
+		elseif (v == 'Tamper' and otherdevices[i] == 'On') then
 			commandArray['SendNotification']='ALARM#ALARM: '..i..' is ON, but nobody is home!#2#default'
+		else
+			commandArray['SendNotification']='ALARM#ALARM: Something is ON, but nobody is home!#2#default'
 		end
 	end
 end
