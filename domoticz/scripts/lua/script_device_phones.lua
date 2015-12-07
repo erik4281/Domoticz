735cc0e264be416360c389c84dd93dbf58c91046
erik4281 <erik4281@users.noreply.github.com>
2015-12-07 21:51:18 +0100

    Update script_device_phones.lua

--- domoticz/scripts/lua/script_device_phones.lua
+++ domoticz/scripts/lua/script_device_phones.lua
@@ -1,19 +1,24 @@
 commandArray = {}
 
-if (otherdevices['iPhoneErik'] == 'On' or otherdevices['iPhoneJinHee'] == 'On') then
-	if (otherdevices['Phones' == 'Off') then
-		commandArray['Phones'] = 'On'
+dc = next(devicechanged)
+ts = tostring(dc)
+
+if (ts:sub(1,6) == 'iPhone') then
+	if (otherdevices['iPhoneErik'] == 'On' or otherdevices['iPhoneJinHee'] == 'On') then
+		if (otherdevices['Phones' == 'Off') then
+			commandArray['Phones'] = 'On'
+		end
+		if (otherdevices['People' == 'Off') then
+			commandArray['People'] = 'On'
+		end
 	end
-	if (otherdevices['People' == 'Off') then
-		commandArray['People'] = 'On'
-	end
-end
-if (otherdevices['iPhoneErik'] == 'Off' and otherdevices['iPhoneJinHee'] == 'Off') then
-	if (otherdevices['Phones' == 'On') then
-		commandArray['Phones'] = 'Off'
-	end
-	if (otherdevices['People' == 'On') then
-		commandArray['People'] = 'Off'
+	if (otherdevices['iPhoneErik'] == 'Off' and otherdevices['iPhoneJinHee'] == 'Off') then
+		if (otherdevices['Phones' == 'On') then
+			commandArray['Phones'] = 'Off'
+		end
+		if (otherdevices['People' == 'On') then
+			commandArray['People'] = 'Off'
+		end
 	end
 end
 
