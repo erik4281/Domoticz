05c5c884de0eb886281cab87c1fc6b5f6abae36a
erik4281 <erik4281@users.noreply.github.com>
2015-12-10 22:30:41 +0100

    Delete git_update.py

--- domoticz/scripts/python/git_update.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-sudo git reset --hard HEAD
-sudo git pull
-sudo chmod -R 750 domoticz/scripts/bash/
-sudo chmod 750 domoticz/scripts/python/check_device_bluetooth.py
-sudo chmod 750 domoticz/scripts/python/check_device_online.py
