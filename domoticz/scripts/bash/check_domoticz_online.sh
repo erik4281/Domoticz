99f0200b33873fa5a361e05eb088ef4a8082da50
erik4281 <erik4281@users.noreply.github.com>
2015-12-10 21:29:54 +0100

    Create check_domoticz_online.sh

---
+++ domoticz/scripts/bash/check_domoticz_online.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# check domoticz
+status=`curl -s -i -H "Accept: application/json" "http://10.0.1.120:8080/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
+if [ $status ]
+then
+echo "Domoticz is al gestart"
+else
+sudo service domoticz.sh stop
+sleep 5
+sudo service domoticz.sh start
+fi
