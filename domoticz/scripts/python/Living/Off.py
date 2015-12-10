3d389d70ba7b3c760f72836552b271f8597c7e30
erik4281 <erik4281@users.noreply.github.com>
2015-12-10 20:43:06 +0100

    Update Off.py

--- domoticz/scripts/python/Living/Off.py
+++ domoticz/scripts/python/Living/Off.py
@@ -1,2 +1,1 @@
-import os
-os.system("./HarmonyHubControl erikvennink@gmail.com ecokey4281 10.0.1.107 start_activity -1")
+os.popen("./HarmonyHubControl erikvennink@gmail.com ecokey4281 10.0.1.107 start_activity -1")
