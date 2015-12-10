066e79dcb8213cab5d89cf4fb6ff1d5adfb85294
erik4281 <erik4281@users.noreply.github.com>
2015-12-10 22:20:21 +0100

    Update git_update.sh

--- domoticz/scripts/bash/git_update.sh
+++ domoticz/scripts/bash/git_update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # git update
 #sudo git reset --hard HEAD
-sudo git pull
+#sudo git pull
 sudo chmod -R 750 domoticz/scripts/bash/
 sudo chmod 750 domoticz/scripts/python/check_device_bluetooth.py
 sudo chmod 750 domoticz/scripts/python/check_device_online.py
