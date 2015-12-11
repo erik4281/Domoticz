feadc62221d127afdd939c574efabfb492f6de4f
erik4281 <erik4281@users.noreply.github.com>
2015-12-11 21:10:44 +0100

    Update Colorloop.sh

--- domoticz/scripts/bash/LivingExtra/Colorloop.sh
+++ domoticz/scripts/bash/LivingExtra/Colorloop.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 200,"sat": 255,"hue":0,"effect":"colorloop"}' http://10.0.1.102/api/erikvennink/lights/1/state
-sleep 20
-curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 200,"sat": 255,"hue":0,"effect":"none"}' http://10.0.1.102/api/erikvennink/lights/1/state
