b072d39b66b0d315f9d4fbb0bc9b8e273d180e6e
erik4281 <erik4281@users.noreply.github.com>
2015-12-11 20:34:42 +0100

    Update Alert.sh

--- domoticz/scripts/bash/LivingExtra/Alert.sh
+++ domoticz/scripts/bash/LivingExtra/Alert.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -s -H "Accept: application/json" -X PUT --data '{"alert":"select","sat":255,"hue":0}' http://10.0.1.102/api/erikvennink/lights/2/state
+#curl -s -H "Accept: application/json" -X PUT --data '{"alert":"select","sat":255,"hue":0}' http://10.0.1.102/api/erikvennink/lights/2/state
+curl -s -H "Accept: application/json" -X PUT --data '{"alert":"select"}' http://10.0.1.102/api/erikvennink/lights/2/state
