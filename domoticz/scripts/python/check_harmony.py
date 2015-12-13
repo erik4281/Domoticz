1286b59993ff3a337d3c83f488d52bf78e886c73
erik4281 <erik4281@users.noreply.github.com>
2015-12-13 21:29:04 +0100

    Update check_harmony.py

--- domoticz/scripts/python/check_harmony.py
+++ domoticz/scripts/python/check_harmony.py
@@ -106,9 +106,9 @@ if lastreported == 0 :
  
 while 1==1:
 #  currentstate = subprocess.call('sudo HarmonyHubControl/HarmonyHubControl erikvennink@gmail.com ecokey4281 '+ device + ' get_current_activity_id_raw > /dev/null', shell=True)
-  currentstate = subprocess.check_output('sudo HarmonyHubControl/HarmonyHubControl erikvennink@gmail.com ecokey4281 '+ device + ' get_current_activity_id_raw', shell=True)
+  currentstate = subprocess.check_output('sudo HarmonyHubControl/HarmonyHubControl erikvennink@gmail.com ecokey4281 '+ device + ' get_current_activity_id_raw', shell=True) + 1
   log (datetime.datetime.now().strftime("%H:%M:%S") + "- " + currentstate)
-  currentstate = int (currentstate)
+#  currentstate = int (currentstate)
   
   if currentstate > 0 : lastsuccess=datetime.datetime.now()
   if currentstate > 0 and currentstate != previousstate and lastreported == 1 : 
