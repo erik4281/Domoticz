1fad39e4b14b3c20a04e25c52f0480666bcb13e2
erik4281 <erik4281@users.noreply.github.com>
2015-12-16 19:46:16 +0100

    Update script_device_modes.lua

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -112,7 +112,7 @@ if (ts == 'ALARM' and devicechanged[dc] == 'On' and otherdevices['People'] == 'O
 elseif (ts == 'ALARM' and devicechanged[dc] == 'Off' and otherdevices['People'] == 'Off') then
 	commandArray['SendNotification']='ALARM#Alarm is OFF!#2#default'
 elseif (ts == 'ALARM') then
-	os.execute(https://api.pilot.patrickferreira.com/uIlZfdCTm3/#SUBJECT/#MESSAGE)
+	os.execute('https://api.pilot.patrickferreira.com/uIlZfdCTm3/#SUBJECT/#MESSAGE')
 end
 
 return commandArray
