d9270ae9c6f0f930fdecb4a93d48e87b97698a85
erik4281 <erik4281@users.noreply.github.com>
2015-12-16 20:19:36 +0100

    Update script_device_modes.lua

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -126,11 +126,11 @@ elseif (ts == 'ALARM') then
 	notJinHee = 'VJsRPzgoPD'
 	notSubject = 'Test'
 	notMessage = 'Message'
-	print(notErik..' - '..notJinHee..' - '..notSubject..' - '..notMessage)
 	--os.execute(curl "https://api.pilot.patrickferreira.com/uIlZfdCTm3/Test/Message123")
 	
-	sMsg = 'curl https://api.pilot.patrickferreira.com/uIlZfdCTm3/Test/Message123'
-	os.execute(sMsg)
+	--sMsg = 'curl https://api.pilot.patrickferreira.com/uIlZfdCTm3/Test/Message123'
+	response = os.execute('curl https://api.pilot.patrickferreira.com/uIlZfdCTm3/Test/Message123')
+	print(notErik..' - '..notJinHee..' - '..notSubject..' - '..notMessage..' = '..response)
 	
 	--notErik = 'uIlZfdCTm3'
 	--notJinHee = 'VJsRPzgoPD'
