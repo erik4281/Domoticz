7dfbc40d5138ef887a35628cd2596c5c74f47cfb
erik4281 <erik4281@users.noreply.github.com>
2015-12-16 21:10:24 +0100

    Update script_device_modes.lua

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -3,6 +3,21 @@ function notify(notSubject, notMessage)
 --	os.execute('https://api.pilot.patrickferreira.com/notJinHee/notSubject/notMessage')
 end
 
+function notifyMe(iPriority, sTitle, sBody, sSound, sDevice)
+   -- iPriority = -2,-1,0,1,2
+   -- sDevice = name of device, empty is all
+   -- Check for optional sound and device parameter
+   if sSound == nil then sSound = '' end
+   if sDevice == nil then sDevice = '' end
+   local sUserKey = '[KEY]'
+   local sToken = '[TOKEN]'
+   local sMsg = 'curl --data "token=' .. sToken .. '&user=' .. sUserKey .. '&title=' .. sTitle .. '&message=' .. sBody .. '&priority=' .. tostring(iPriority) .. '&sound=' .. sSound .. '&device=' .. sDevice .. '" https://api.pushover.net/1/messages.json'
+   os.execute(sMsg)
+end
+
+
+
+
 commandArray = {}
 
 dc = next(devicechanged)
