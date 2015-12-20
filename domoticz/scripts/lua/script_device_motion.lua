052e99055ebb39fb846e9dd92115ace3f69e9551
erik4281 <erik4281@users.noreply.github.com>
2015-12-20 09:07:24 +0100

    Update script_device_motion.lua

--- domoticz/scripts/lua/script_device_motion.lua
+++ domoticz/scripts/lua/script_device_motion.lua
@@ -12,6 +12,7 @@ if (ts:sub(1,6) == 'Motion' and presence == 'On') then
 	sl = nil
 	sm = nil
 	wakeuptimer = false
+	print(wakeuptimer)
 	if (sc == 'Living') then
 		sc = 'Living'
 		sd = 'Dining'
@@ -41,6 +42,7 @@ if (ts:sub(1,6) == 'Motion' and presence == 'On') then
 		sc = 'Bathroom'
 		sd = 'Hallway'
 	end
+	print(wakeuptimer)
 	mc = 'Motion'..sc
 	cbr = tonumber(otherdevices_svalues['Brightness'..sc])
 	cbrt = tonumber(uservariables['BrightnessTrigger'..sc])
