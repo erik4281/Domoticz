5fd49ee152eda7aae1809252673d19f708aa8d27
erik4281 <erik4281@users.noreply.github.com>
2015-12-20 18:30:28 +0100

    Update script_time_motion.lua

--- domoticz/scripts/lua/script_time_motion.lua
+++ domoticz/scripts/lua/script_time_motion.lua
@@ -44,13 +44,13 @@ for i, v in pairs(otherdevices) do
 		elseif (sc == 'Kitchen') then
 			sc = 'Kitchen'
 			sl = 'KitchenExtra'
-			if (otherdevices[v..sc] == 'On' or timedifference(otherdevices_lastupdate[v..sc]) < 60 and otherdevices['FanHigh'] == 'Off') then
+			if (otherdevices[v..sc] == 'On' or timedifference(otherdevices_lastupdate[v..sc]) < 60 and otherdevices['FanMax'] == 'Off') then
 				commandArray['Variable:FanMotionOff'] = tostring(0)
 				commandArray['Variable:FanMotionOn'] = tostring(uservariables['FanMotionOn'] + 1)
 				if (uservariables['FanMotionOn'] > 5 and otherdevices['FanHigh'] == 'Off') then
 					commandArray['FanHigh'] = 'On'
 				end
-			elseif (otherdevices[v..sc] == 'Off' and timedifference(otherdevices_lastupdate[v..sc]) > 60 and otherdevices['FanHigh'] == 'On') then
+			elseif (otherdevices[v..sc] == 'Off' and timedifference(otherdevices_lastupdate[v..sc]) > 60 and otherdevices['FanMax'] == 'On') then
 				commandArray['Variable:FanMotionOn'] = tostring(0)
 				commandArray['Variable:FanMotionOff'] = tostring(uservariables['FanMotionOff'] + 1)
 				if (tonumber(uservariables['FanMotionOff']) > 15) then
