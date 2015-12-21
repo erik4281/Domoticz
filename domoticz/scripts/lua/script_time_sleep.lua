a62a6f51fd17077756a22047f9e7b209231e91bd
erik4281 <erik4281@users.noreply.github.com>
2015-12-21 19:58:11 +0100

    Update script_time_sleep.lua

--- domoticz/scripts/lua/script_time_sleep.lua
+++ domoticz/scripts/lua/script_time_sleep.lua
@@ -47,6 +47,13 @@ for i, v in pairs(otherdevices) do
 			commandArray[switchsleep]='On'
 		end
 	end
+	print (presence)
+	print (sleep)
+	print (wakeup)
+	print (timenumber)
+	print (wakeuptime)
+	print (otherdevices['SwitchBedroom'])
+	print (weekday)
 	if (presence == 'On' and sleep == 'On' and wakeup == 'On' and timenumber == wakeuptime and otherdevices['SwitchBedroom'] == 'Off' and (weekday > 0 and weekday < 6)) then
 		commandArray['Variable:WakeUpLightOn'] = tostring(1)
 		scriptfolder = "/home/pi/domoticz/scripts/bash/"
