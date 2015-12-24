cd19b646c057e2d0e1c80d9b6e722b7e689d7a23
erik4281 <erik4281@users.noreply.github.com>
2015-12-24 22:43:31 +0100

    Update Christmas.sh

--- domoticz/scripts/bash/LivingExtra/Christmas.sh
+++ domoticz/scripts/bash/LivingExtra/Christmas.sh
@@ -16,3 +16,28 @@ curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat"
 sleep 5
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":0,"transitiontime":50}' http://10.0.1.102/api/erikvennink/lights/5/state
 sleep 5
+
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 100,"hue":50000,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":27000,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":0,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 100,"hue":50000,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":27000,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":0,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 100,"hue":50000,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":27000,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":0,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 100,"hue":50000,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":27000,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":0,"transitiontime":2}' http://10.0.1.102/api/erikvennink/lights/5/state
+sleep 0.2
