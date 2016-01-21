0bcbcf201eb942b965c6d45a03d5080a8ed94695
erik4281 <erik4281@users.noreply.github.com>
2016-01-21 20:50:31 +0100

    Update git_update.sh

--- domoticz/scripts/bash/git_update.sh
+++ domoticz/scripts/bash/git_update.sh
@@ -2,5 +2,6 @@
 #git update
 sudo git reset --hard HEAD
 sudo git pull
-sudo chmod -R 750 /home/pi/domoticz/scripts/bash/
-sudo chmod -R 750 /home/pi/domoticz/scripts/python/
+sudo chmod -R 755 /home/pi/domoticz/scripts/bash/
+sudo chmod -R 755 /home/pi/domoticz/scripts/lua/
+sudo chmod -R 755 /home/pi/domoticz/scripts/python/
