eb2e777336c2dcec2eb3798a241cb6e98841e6ee
erik4281 <erik4281@users.noreply.github.com>
2016-01-24 17:22:41 +0100

    Update check_device_online.py

--- domoticz/scripts/python/check_device_online.py
+++ domoticz/scripts/python/check_device_online.py
@@ -105,7 +105,7 @@ if lastreported == 0 :
   log (datetime.datetime.now().strftime("%H:%M:%S") + "- according to domoticz, " + device + " is offline")
  
 while 1==1:
-  currentstate = subprocess.call('ping -q -c1 -W 1 '+ device + ' > /dev/null', shell=True)
+  currentstate = subprocess.call('sudo ping -q -c1 -W 1 '+ device + ' > /dev/null', shell=True)
  
   if currentstate == 0 : lastsuccess=datetime.datetime.now()
   if currentstate == 0 and currentstate != previousstate and lastreported == 1 : 
