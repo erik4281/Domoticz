7c0e43b4aa0689cd3ab0da6b627d611e91ae4f00
erik4281 <erik4281@users.noreply.github.com>
2016-01-29 18:12:45 +0100

    Update WakeUp.sh

--- domoticz/scripts/bash/Bedroom/WakeUp.sh
+++ domoticz/scripts/bash/Bedroom/WakeUp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":1000,"transitiontime":10}' http://10.0.1.102/api/erikvennink/lights/6/state
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 150,"hue":15000,"transitiontime":1800}' http://10.0.1.102/api/erikvennink/lights/16/state
 sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 255,"sat": 255,"hue":25000,"transitiontime":1800}' http://10.0.1.102/api/erikvennink/lights/17/state
