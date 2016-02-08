7c295d759e88b81e7f8d7599b214facc0c381cef
erik4281 <erik4281@users.noreply.github.com>
2016-02-08 19:57:05 +0100

    Update git_update.sh

--- domoticz/scripts/bash/git_update.sh
+++ domoticz/scripts/bash/git_update.sh
@@ -5,6 +5,3 @@ sudo git pull
 sudo chmod -R 755 /home/pi/domoticz/scripts/bash/
 sudo chmod -R 755 /home/pi/domoticz/scripts/lua/
 sudo chmod -R 755 /home/pi/domoticz/scripts/python/
-sudo npm update -g homebridge-edomoticz
-sudo forever restart /usr/local/lib/node_modules/homebridge/bin/homebridge
-tail -f `ls -t ~/.forever/*.log | grep -v '^d' | head -n1`
