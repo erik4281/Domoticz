36605ca66f06e5fe3baabc5df1b56f83b078e802
erik4281 <erik4281@users.noreply.github.com>
2016-02-29 20:44:47 +0100

    Update script_device_modes.lua

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -43,10 +43,10 @@ end
 
 if (ts == 'SleepMode') then
 	if (devicechanged[dc] == 'On') then
-		commandArray['Bedroom Humidifier'] = 'On'
+		commandArray['SwitchHumidifier'] = 'On'
 		commandArray[1] = {['UpdateDevice'] = "41|0|18"}
 	elseif (devicechanged[dc] == 'Off') then
-		commandArray['Bedroom Humidifier'] = 'Off'
+		commandArray['SwitchHumidifier'] = 'Off'
 		commandArray[1] = {['UpdateDevice'] = "41|0|22"}
 	end
 	for i, v in pairs(otherdevices) do
@@ -56,42 +56,6 @@ if (ts == 'SleepMode') then
 			scriptfolder = "/home/pi/domoticz/scripts/bash/"
 			if (otherdevices[i] == 'On') then
 				commandArray[i] = 'On'
-				--timenumber = tonumber(os.date("%H")..os.date("%M"))
-				--time1 = tonumber(uservariables['Timer'..sc..'1'])
-				--time2 = tonumber(uservariables['Timer'..sc..'2'])
-				--time3 = tonumber(uservariables['Timer'..sc..'3'])
-				--time4 = tonumber(uservariables['Timer'..sc..'4'])
-				--if (time1) then
-				--	scene = 0
-				--else
-				--	scene = 1
-				--end
-				--if (scene == 0 and time1) then
-				--	if (timenumber >= time1) then
-				--		scene = 1
-				--	end
-				--end
-				--if (scene == 1 and time2) then
-				--	if (timenumber >= time2) then
-				--		scene = 2
-				--	end
-				--end
-				--if (scene == 2 and time3) then
-				--	if (timenumber >= time3) then
-				--		scene = 3
-				--	end
-				--end
-				--if (scene == 3 and time4) then
-				--	if (timenumber >= time4) then
-				--		scene = 4
-				--	end
-				--end
-				--if (devicechanged[dc] == 'On') then
-				--	scene = 9
-				--end
-				--scene = scene..'Slow.sh'
-				--print ('Switch triggered: '..scriptfolder..sc..'/'..scene)
-				--os.execute (scriptfolder..sc..'/'..scene)
 			end
 		end
 	end
