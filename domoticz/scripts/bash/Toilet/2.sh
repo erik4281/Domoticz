bfb53e05be1cb028744eb1953d4f7ad5f42cbc83
erik4281 <erik4281@users.noreply.github.com>
2016-03-18 08:54:02 +0100

    Update 2.sh

--- domoticz/scripts/bash/Toilet/2.sh
+++ domoticz/scripts/bash/Toilet/2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 200,"transitiontime":5}' http://10.0.1.102/api/erikvennink/lights/22/state
+sleep .05
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 200,"transitiontime":5}' http://10.0.1.102/api/erikvennink/lights/22/state
