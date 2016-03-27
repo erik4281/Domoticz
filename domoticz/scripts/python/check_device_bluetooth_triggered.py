e341f8a821a7b8e982ec01075e2bb0e093cb2e5d
erik4281 <erik4281@users.noreply.github.com>
2016-03-27 12:19:04 +0200

    Update check_device_bluetooth_triggered.py

--- domoticz/scripts/python/check_device_bluetooth_triggered.py
+++ domoticz/scripts/python/check_device_bluetooth_triggered.py
@@ -140,8 +140,8 @@ log (datetime.datetime.now().strftime("%H:%M:%S") + "- checking trigger done")
 log (datetime.datetime.now().strftime("%H:%M:%S") + "- now starting loop!")
 
 while 1==1:
-  log (datetime.datetime.now().strftime("%H:%M:%S") + "- Now triggering BT-ping with trigger state " + checktrigger)
-  if checktrigger == 1 : currentstate = subprocess.call('sudo l2ping -c 1 '+ device + ' > /dev/null', shell=True)
+  # log (datetime.datetime.now().strftime("%H:%M:%S") + "- Now triggering BT-ping with trigger state " + checktrigger)
+  # if checktrigger == 1 : currentstate = subprocess.call('sudo l2ping -c 1 '+ device + ' > /dev/null', shell=True)
 
   if currentstate == 0 : lastsuccess=datetime.datetime.now()
   if currentstate == 0 and currentstate != previousstate and lastreported == 1 : 
