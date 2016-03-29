bb7f2fddf4232bc2bd57418a01f27046f138859a
erik4281 <erik4281@users.noreply.github.com>
2016-03-29 20:38:14 +0200

    Update script_device_wake_up.lua

--- domoticz/scripts/lua/script_device_wake_up.lua
+++ domoticz/scripts/lua/script_device_wake_up.lua
@@ -4,6 +4,8 @@ dc = next(devicechanged)
 ts = tostring(dc)
 sleep = otherdevices['SleepMode']
 switchsleep = 'SleepMode'
+modesleep = otherdevices['SleepMode']
+modeswitchsleep = 'SleepMode'
 presence = otherdevices['People']
 
 if (ts:sub(1,6) == 'Motion' and presence == 'On' and sleep == 'On') then
@@ -15,6 +17,19 @@ if (ts:sub(1,6) == 'Motion' and presence == 'On' and sleep == 'On') then
 		if (sc == 'Living' or sc == 'Dining' or sc == 'Kitchen') then
 			print ("Waking up")
 			commandArray[switchsleep] = 'Off'
+		end
+	end
+end
+
+if (ts:sub(1,6) == 'Motion' and presence == 'On' and modesleep == 'On') then
+	timenumber = tonumber(os.date("%H")..os.date("%M"))
+	wakestart = 0600
+	wakestop = 1200
+	if (timenumber >= wakestart and timenumber < wakestop) then
+		sc = ts:sub(7)
+		if (sc == 'Living' or sc == 'Dining' or sc == 'Kitchen') then
+			print ("Waking up")
+			commandArray[modeswitchsleep] = 'Off'
 		end
 	end
 end
