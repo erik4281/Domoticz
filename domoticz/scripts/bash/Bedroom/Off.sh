8fadd248156a5b4c0eb095922441cf2a97ccdccb
erik4281 <erik4281@users.noreply.github.com>
2016-03-31 11:23:31 +0200

    Update Off.sh

--- domoticz/scripts/bash/Bedroom/Off.sh
+++ domoticz/scripts/bash/Bedroom/Off.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":false,"transitiontime":150}' http://10.0.1.102/api/erikvennink/lights/16/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":false,"transitiontime":150}' http://10.0.1.102/api/erikvennink/lights/17/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":false}' http://10.0.1.102/api/erikvennink/lights/19/state
