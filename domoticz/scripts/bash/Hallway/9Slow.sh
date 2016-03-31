982550dc5a08bbd99fc2ec3f270f9c112108ffb6
erik4281 <erik4281@users.noreply.github.com>
2016-03-31 11:27:28 +0200

    Update 9Slow.sh

--- domoticz/scripts/bash/Hallway/9Slow.sh
+++ domoticz/scripts/bash/Hallway/9Slow.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 100,"transitiontime":150}' http://10.0.1.102/api/erikvennink/lights/13/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 100,"transitiontime":150}' http://10.0.1.102/api/erikvennink/lights/14/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 100,"transitiontime":150}' http://10.0.1.102/api/erikvennink/lights/15/state
