af63385e9b2c5fc2d97e3b962b9a505dd5976daf
erik4281 <erik4281@users.noreply.github.com>
2016-03-31 11:22:48 +0200

    Create Sleep.sh

---
+++ domoticz/scripts/bash/Bedroom/Sleep.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 100,"sat": 255,"hue":1,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/16/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":false,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/17/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":false}' http://10.0.1.102/api/erikvennink/lights/19/state
