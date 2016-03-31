f42e3357b368d723930c21f266533d6142774234
erik4281 <erik4281@users.noreply.github.com>
2016-03-31 12:03:54 +0200

    Create Dimming.sh

---
+++ domoticz/scripts/bash/Kitchen/Dimming.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 50,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/8/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 50,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/9/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 50,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/10/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":false}' http://10.0.1.102/api/erikvennink/lights/25/state
