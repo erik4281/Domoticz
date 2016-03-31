e82cd632d074a536da51d023f395bf4f4220df1a
erik4281 <erik4281@users.noreply.github.com>
2016-03-31 11:37:40 +0200

    Update 3Slow.sh

--- domoticz/scripts/bash/Living/3Slow.sh
+++ domoticz/scripts/bash/Living/3Slow.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 180,"sat": 180,"hue":15000,"transitiontime":600}' http://10.0.1.102/api/erikvennink/lights/2/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 180,"sat": 180,"hue":15000,"transitiontime":600}' http://10.0.1.102/api/erikvennink/lights/3/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 180,"sat": 180,"hue":15000,"transitiontime":600}' http://10.0.1.102/api/erikvennink/lights/4/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 180,"sat": 255,"hue":1,"transitiontime":600}' http://10.0.1.102/api/erikvennink/lights/5/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 180,"sat": 180,"hue":15000,"transitiontime":600}' http://10.0.1.102/api/erikvennink/lights/6/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 180,"sat": 180,"hue":15000,"transitiontime":600}' http://10.0.1.102/api/erikvennink/lights/7/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 180,"sat": 180,"hue":15000,"transitiontime":600}' http://10.0.1.102/api/erikvennink/lights/27/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 180,"sat": 255,"hue":50000,"transitiontime":600}' http://10.0.1.102/api/erikvennink/lights/28/state
-sleep .05
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 180,"sat": 255,"hue":0,"transitiontime":600}' http://10.0.1.102/api/erikvennink/lights/29/state
