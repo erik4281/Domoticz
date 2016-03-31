de7f832a6c83783779c4b3d0ee9359c6def49c66
erik4281 <erik4281@users.noreply.github.com>
2016-03-31 11:41:52 +0200

    Delete Alert.sh

--- domoticz/scripts/bash/LivingExtra/Alert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -s -H "Accept: application/json" -X PUT --data '{"alert":"lselect"}' http://10.0.1.102/api/erikvennink/lights/1/state
-curl -s -H "Accept: application/json" -X PUT --data '{"alert":"lselect"}' http://10.0.1.102/api/erikvennink/lights/5/state
-curl -s -H "Accept: application/json" -X PUT --data '{"alert":"lselect"}' http://10.0.1.102/api/erikvennink/lights/28/state
-curl -s -H "Accept: application/json" -X PUT --data '{"alert":"lselect"}' http://10.0.1.102/api/erikvennink/lights/29/state
