c47837ddd98d3d21b3cb44f5bc77be400d75439d
erik4281 <erik4281@users.noreply.github.com>
2016-03-31 11:34:25 +0200

    Update 0.sh

--- domoticz/scripts/bash/Living/0.sh
+++ domoticz/scripts/bash/Living/0.sh
@@ -3,7 +3,8 @@ curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat"
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/3/state
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/4/state
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 255,"hue":1,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/5/state
-curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/6/statecurl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/7/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/6/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/7/state
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 150,"hue":15000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/27/state
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 255,"hue":50000,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/28/state
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 150,"sat": 255,"hue":0,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/29/state
