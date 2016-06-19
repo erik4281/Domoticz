7a21e4f86ffd1756922a3607f9459f30587dc0a3
erik4281 <erik4281@users.noreply.github.com>
2016-06-19 19:48:49 +0200

    Update 2Slow.sh

--- domoticz/scripts/bash/LivingExtra/2Slow.sh
+++ domoticz/scripts/bash/LivingExtra/2Slow.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 200,"sat": 255,"hue":0,"transitiontime":300,"effect":"none"}' http://10.0.1.102/api/erikvennink/lights/1/state
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 200,"transitiontime":300}' http://10.0.1.102/api/erikvennink/lights/12/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true}' http://10.0.1.102/api/erikvennink/lights/35/state
