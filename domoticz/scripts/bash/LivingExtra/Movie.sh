9955680f683e6f78962b40788c8d1d216187a609
erik4281 <erik4281@users.noreply.github.com>
2016-06-19 19:50:21 +0200

    Update Movie.sh

--- domoticz/scripts/bash/LivingExtra/Movie.sh
+++ domoticz/scripts/bash/LivingExtra/Movie.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 50,"sat": 255,"hue":0,"transitiontime":20,"effect":"none"}' http://10.0.1.102/api/erikvennink/lights/1/state
 curl -s -H "Accept: application/json" -X PUT --data '{"on":true,"bri": 200,"transitiontime":20}' http://10.0.1.102/api/erikvennink/lights/12/state
+curl -s -H "Accept: application/json" -X PUT --data '{"on":true}' http://10.0.1.102/api/erikvennink/lights/35/state
