f43d02b80edc13f46f2955e9e1fc0b7180bf8f33
erik4281 <erik4281@users.noreply.github.com>
2016-06-26 10:04:40 +0200

    Update script_device_wake_up.lua

--- domoticz/scripts/lua/script_device_wake_up.lua
+++ domoticz/scripts/lua/script_device_wake_up.lua
@@ -8,7 +8,7 @@ presence = otherdevices['People']
 
 if (ts:sub(1,6) == 'Motion' and presence == 'On' and sleep == 'On') then
 	timenumber = tonumber(os.date("%H")..os.date("%M"))
-	wakestart = 0400
+	wakestart = 0700
 	wakestop = 1200
 	if (timenumber >= wakestart and timenumber < wakestop) then
 		sc = ts:sub(7)
