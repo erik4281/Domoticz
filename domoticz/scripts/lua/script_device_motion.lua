c57ad925e83b8a96f4799efd1aa9bf3de26b6841
erik4281 <erik4281@users.noreply.github.com>
2016-09-10 09:10:15 +0200

    Update script_device_motion.lua

--- domoticz/scripts/lua/script_device_motion.lua
+++ domoticz/scripts/lua/script_device_motion.lua
@@ -31,7 +31,7 @@ if (ts:sub(1,6) == 'Motion' and presence == 'On') then
 		if (otherdevices['ModeSleep'] == 'On') then
 			commandArray['ModeStandard'] = 'On'
 		end
-		sl = 'DoorOpened'
+		--sl = 'DoorOpened'
 	elseif (sc == 'Bedroom') then
 		sc = 'Bedroom'
 		sl = 'Humidifier'
@@ -106,14 +106,14 @@ if (ts:sub(1,6) == 'Motion' and presence == 'Off' and (devicechanged[dc] == 'On'
 		commandArray['People'] = 'On'
 		commandArray['Variable:AlarmTimer'] = tostring(10)
 		commandArray['Variable:PeopleTimer'] = tostring(0)
-		commandArray['SwitchDoorOpened'] = 'On'
+		--commandArray['SwitchDoorOpened'] = 'On'
 	elseif (sc == 'Bedroom') then
 		print ('Motion in the bedroom, people are present, switching on light and setting mode to present')
 		commandArray['SwitchBedroom'] = 'On'
 		commandArray['People'] = 'On'
 		commandArray['Variable:AlarmTimer'] = tostring(10)
 		commandArray['Variable:PeopleTimer'] = tostring(0)
-		commandArray['SwitchDoorOpened'] = 'On'
+		--commandArray['SwitchDoorOpened'] = 'On'
 	else
 		if (otherdevices['ALARM'] == 'Off') then
 			commandArray['ALARM'] = 'On'
