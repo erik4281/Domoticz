2773bd7b05084381d7fc3c9d8f98f277f4ce70f0
erik4281 <erik4281@users.noreply.github.com>
2016-09-11 07:12:31 +0200

    Update script_device_modes.lua

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -3,26 +3,6 @@ commandArray = {}
 dc = next(devicechanged)
 ts = tostring(dc)
 
-if (ts == 'FanSwitch2') then
-	if (otherdevices['FanSwitch2'] == 'Off') then
-		if (otherdevices['People'] == 'On' and otherdevices['ALARM'] == 'Off') then
-			commandArray['FanSwitch2'] = 'On'
-		end	
-	elseif (otherdevices['FanSwitch2'] == 'On') then
-		if (otherdevices['People'] == 'Off') then
-			commandArray['FanSwitch2'] = 'Off'
-		end	
-	end
-end
-
-if (ts == 'FanSwitch3') then
-	if (otherdevices['FanSwitch3'] == 'On') then
-		if (otherdevices['People'] == 'Off') then
-			commandArray['FanSwitch3'] = 'Off'
-		end	
-	end
-end
-
 --if (ts:sub(1,6) == 'iPhone') then
 --	print((otherdevices_lastupdate[dc]))
 --	if (devicechanged[dc] == 'On') then
@@ -115,27 +95,6 @@ if (ts == 'People') then
 			end
 		end
 	end
-end
-
-if (ts == 'NestActive') then
-	if (devicechanged[dc] == 'On') then
-		commandArray['NestAway'] = 'Off'
-	elseif (devicechanged[dc] == 'Off') then
-		commandArray['NestAway'] = 'On'
-	end
-end
-
-if (ts == 'ALARM' and devicechanged[dc] == 'On') then
-	for i, v in pairs(otherdevices) do
-		v = i:sub(1,6)
-		if (v == 'Motion' and otherdevices[i] == 'On') then
-			commandArray['SendNotification']='ALARM#'..i..' is ON!#1#siren'
-		elseif (v == 'Motion' and otherdevices[i] == 'Open') then
-			commandArray['SendNotification']='ALARM#'..i..' is OPEN!#1#siren'
-		end
-	end
-elseif (ts == 'ALARM' and devicechanged[dc] == 'Off') then
-	commandArray['SendNotification']='ALARM#Alarm is OFF#1#siren'
 end
 
 return commandArray
