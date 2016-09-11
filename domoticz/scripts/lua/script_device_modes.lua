554e70eef1e16f113637d44ff67b1862e22ab69f
erik4281 <erik4281@users.noreply.github.com>
2016-09-11 07:14:03 +0200

    Update script_device_modes.lua

--- domoticz/scripts/lua/script_device_modes.lua
+++ domoticz/scripts/lua/script_device_modes.lua
@@ -62,39 +62,4 @@ if (ts:sub(1,4) == 'Mode') then
 	end
 end
 
-if (ts == 'People') then
-	if (devicechanged[dc] == 'On') then
-		if (otherdevices['NestActive'] == 'Off') then
-			commandArray[1] = {['UpdateDevice'] = "41|0|22"}
-			commandArray['NestActive'] = 'On'
-		end
-		commandArray['FanSwitch2'] = 'On'
-		commandArray['SendNotification']='HOME#HOME mode activated#0#intermission'
-		commandArray['ModeStandard'] = 'On'
-	elseif (devicechanged[dc] == 'Off') then
-		if (otherdevices['NestActive'] == 'On') then
-			commandArray[1] = {['UpdateDevice'] = "41|0|22"}
-			commandArray['NestActive'] = 'Off'
-		end
-		commandArray['FanSwitch3'] = 'Off'
-		commandArray['SendNotification']='HOME#AWAY mode activated#0#intermission'
-		for i, v in pairs(otherdevices) do
-			v = i:sub(1,6)
-			if (v == 'Switch') then
-				if (otherdevices[i] == 'On') then
-					commandArray[i] = 'Off'
-				end
-			end
-		end
-		for j, w in pairs(otherdevices) do
-			w = j:sub(1,4)
-			if (w == 'Mode') then
-				if (otherdevices[j] == 'On') then
-					commandArray[j] = 'Off'
-				end
-			end
-		end
-	end
-end
-
 return commandArray
