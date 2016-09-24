1f7b9793f2f7c0e8ab52fd615ed69199dd88fa77
erik4281 <erik4281@users.noreply.github.com>
2016-09-24 13:41:42 +0200

    Update script_device_people.lua

--- domoticz/scripts/lua/script_device_people.lua
+++ domoticz/scripts/lua/script_device_people.lua
@@ -33,23 +33,23 @@ if (ts == 'People') then
 		print ('Coolingvariable OFF')
 		commandArray['SendNotification']='HOME#AWAY mode activated#0#intermission'
 		print ('Notification sent')
-		for i, v in pairs(otherdevices) do
-			v = i:sub(1,6)
-			if (v == 'Switch') then
-				if (otherdevices[i] == 'On') then
-					commandArray[i] = 'Off'
-				end
-			end
-		end
+--		for i, v in pairs(otherdevices) do
+--			v = i:sub(1,6)
+--			if (v == 'Switch') then
+--				if (otherdevices[i] == 'On') then
+--					commandArray[i] = 'Off'
+--				end
+--			end
+--		end
 		print ('Switches OFF')
-		for j, w in pairs(otherdevices) do
-			w = j:sub(1,4)
-			if (w == 'Mode') then
-				if (otherdevices[j] == 'On') then
-					commandArray[j] = 'Off'
-				end
-			end
-		end
+--		for j, w in pairs(otherdevices) do
+--			w = j:sub(1,4)
+--			if (w == 'Mode') then
+--				if (otherdevices[j] == 'On') then
+--					commandArray[j] = 'Off'
+--				end
+--			end
+--		end
 		print ('Modes OFF')
 	end
 end
