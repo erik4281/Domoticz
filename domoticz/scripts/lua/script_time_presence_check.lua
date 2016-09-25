a14187e847815e70005a775418b468277c10b4ed
erik4281 <erik4281@users.noreply.github.com>
2016-09-25 11:59:25 +0200

    Update script_time_presence_check.lua

--- domoticz/scripts/lua/script_time_presence_check.lua
+++ domoticz/scripts/lua/script_time_presence_check.lua
@@ -36,7 +36,7 @@ print ('Motion'..sc..' = 2, motionsensor ON or updated in the last 30 seconds. O
 print ('Motion'..sc..' = 2, motionsensor ON or updated in the last 30 seconds. Only valid with ALARM ON, but out of first 60 seconds of ALARM being switched.')
 			end
 		end
-print ('PeopleTimer: '..tostring(uservariables['PeopleTimer']))
+print ('PeopleTimer: '..tostring(uservariables['PeopleTimer'])..', Motion: '..motion)
 		if (motion == 0) then
 			commandArray['Variable:PeopleTimer'] = tostring(uservariables['PeopleTimer'] + 1)
 			if (uservariables['PeopleTimer'] > 15) then
